930eac7e4003996d6f107d91ebb42dece0e8095a
NicolasIUT <ferrayen@3il.fr>
2023-02-15 09:08:57 +0100

    Ajout du test qui échoue

---
+++ TestFizzBuzz.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+
+public class TestFizzBuzz {
+
+	@Test
+	void testFizzBuzz1() {
+		assertEquals("1", FizzBuzz.fizzBuzz(1));
+	}
+
+}
