e7c947cd5c20227ae09c2c7730f471d329a51689
NicolasIUT <ferrayen@3il.fr>
2023-02-15 09:22:09 +0100

    Ajout des autres tests, modifications de FizzBuzz et refactoring de la classe

--- TestFizzBuzz.java
+++ TestFizzBuzz.java
@@ -1,13 +1,43 @@
-import static org.junit.Assert.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-public class TestFizzBuzz {
-
+class TestFizzBuzz {
+	
 	@Test
 	void testFizzBuzz1() {
 		assertEquals("1", FizzBuzz.fizzBuzz(1));
+	}
+	
+	@Test
+	void testFizzBuzz2() {
+		assertEquals("2", FizzBuzz.fizzBuzz(2));
+	}
+	
+	@Test
+	void testFizzBuzz3() {
+		assertEquals("Fizz", FizzBuzz.fizzBuzz(3));
+	}
+	
+	@Test
+	void testFizzBuzz4() {
+		assertEquals("4", FizzBuzz.fizzBuzz(4));
+	}
+	
+	@Test
+	void testFizzBuzz5() {
+		assertEquals("Buzz", FizzBuzz.fizzBuzz(5));
+	}
+	
+	@Test
+	void testFizzBuzz15() {
+		assertEquals("FizzBuzz", FizzBuzz.fizzBuzz(15));
+	}
+	
+	@Test
+	void testFizzBuzzMoins1() {
+		assertEquals("-1", FizzBuzz.fizzBuzz(-1));
 	}
 
 }
